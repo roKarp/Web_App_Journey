fe9e3fefcc860b2308b8ee85cfbe87f13267ccec
Roope Karppinen <roope.karppinen@aalto.fi>
2023-05-26 13:08:56 +0300

    added pagination to stations page

--- front/src/components/Stations.svelte
+++ front/src/components/Stations.svelte
@@ -1,14 +1,16 @@
 <script>
+  export let page;
   const getStations = async () => {
     const response = await fetch("/api/stations");
     return await response.json();
   };
-
+  let p = parseInt(page);
   let stationsPromise = getStations();
 </script>
 
 <ul>
   <li><a href="/journeys">List of Journeys (first page)</a></li>
+  <li><a href="/stations">List of Stations (first page)</a></li>
 </ul>
 <h1>Stations</h1>
 {#await stationsPromise}
@@ -17,20 +19,34 @@
   {#if stations.length == 0}
     <p>No stations found</p>
   {:else}
-    <ol>
+  {#if Number.isInteger(p)}
+  {#if Math.ceil(stations.length /20) < p}
+  <p>The accessed page has no more values</p>
+  <p>Go back to the <a href="/stations?page=1">start</a></p>
+  {/if}
+  {#if p!= 1}
+  <p class="a"><a href="/stations?page={p - 1}">Previous page</a></p>
+  {/if}
+  {#if Math.ceil(stations.length / 20) >= p  + 1}
+    <p class="b"><a href="/stations?page={p + 1}">Next page</a></p>
+  {/if}
+    <ul>
       <h3 class="b">Station Name/Address</h3>
-      {#each stations as station}
+      {#each stations as station, i}
+      {#if i < p*20 && i >=0 + 20*(p - 1)}
         <li class="a">
           {station.s_name} <a href="/station?id={station.id}">Info</a>
         </li>
+      {/if}
       {/each}
-    </ol>
+    </ul>
+    {/if}
   {/if}
 {/await}
 
 <style>
   :global(body) {
-    background-color: brown;
+    background-color: rgb(199, 106, 18);
     color: black;
   }
 
@@ -48,5 +64,11 @@
   li.a {
     margin-right: 10px;
     margin-left: 10px;
+  }
+  p.a {
+    text-align: left;
+  }
+  p.b {
+    text-align: right;
   }
 </style>
